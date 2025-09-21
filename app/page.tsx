defcc3f1c69e84d39974a90df0a3590566dd4fd4
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-09-21 18:31:28 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -168,13 +168,13 @@ function HeroSection() {
                     </div>
                     <div>
                       <div className="text-sm font-medium text-gray-900">Voice Control</div>
-                      <div className="text-xs text-gray-600">Hey Siri</div>
+                      <div className="text-xs text-gray-600">Just ask Siri</div>
                     </div>
                   </div>
                 </div>
               </motion.div>
 
-              {/* Top right card - Energy Saving */}
+              {/* Top right card - HomeKey */}
               <motion.div
                 initial={{ opacity: 0, x: 20, y: 20 }}
                 animate={{ opacity: 1, x: 0, y: 0 }}
@@ -185,12 +185,12 @@ function HeroSection() {
                   <div className="flex items-center space-x-2">
                     <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center">
                       <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd" />
+                        <path fillRule="evenodd" d="M18 8a6 6 0 01-7.743 5.743L10 14l-2.257-.257A6 6 0 1118 8zm-2 0a4 4 0 11-8 0 4 4 0 018 0zm-5-1.757l.414.414a1 1 0 01-1.414 1.414L9.586 8a1 1 0 010-1.414l.414-.414a1 1 0 011.414 1.414L11 8.414V11a1 1 0 11-2 0V8.414z" clipRule="evenodd" />
                       </svg>
                     </div>
                     <div>
-                      <div className="text-sm font-medium text-gray-900">Energy Saving</div>
-                      <div className="text-xs text-gray-600">-23% usage</div>
+                      <div className="text-sm font-medium text-gray-900">HomeKey</div>
+                      <div className="text-xs text-gray-600">Tap. You're home.</div>
                     </div>
                   </div>
                 </div>
@@ -207,12 +207,13 @@ function HeroSection() {
                   <div className="flex items-center space-x-2">
                     <div className="w-8 h-8 bg-red-500 rounded-full flex items-center justify-center">
                       <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                        <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
                       </svg>
                     </div>
                     <div>
                       <div className="text-sm font-medium text-gray-900">Security</div>
-                      <div className="text-xs text-gray-600">All secure</div>
+                      <div className="text-xs text-gray-600">See who's here</div>
                     </div>
                   </div>
                 </div>
@@ -229,12 +230,12 @@ function HeroSection() {
                   <div className="flex items-center space-x-2">
                     <div className="w-8 h-8 bg-purple-500 rounded-full flex items-center justify-center">
                       <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
+                        <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd" />
                       </svg>
                     </div>
                     <div>
                       <div className="text-sm font-medium text-gray-900">Automation</div>
-                      <div className="text-xs text-gray-600">47 devices</div>
+                      <div className="text-xs text-gray-600">Set the mood</div>
                     </div>
                   </div>
                 </div>
